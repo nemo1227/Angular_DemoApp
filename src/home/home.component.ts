31e55ede4df94c2ad1612e37c6b2ce4b9ffd25ef
nemo1227 <neerajmore27@gmail.com>
2025-04-09 23:01:04 +0530

    Finalised one data element

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../app/housinglocation';
 @Component({
   selector: 'app-home',
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Nemo Home',
+    city: 'Pune',
+    state: 'Maharashtra',
+    photo: 'home.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundary: false,
+  };
 }
--- src/housing-location/housing-location.component.ts
+++ src/housing-location/housing-location.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../app/housinglocation';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: Housinglocation;
 }
+// '!' because the input is expecting the value to be passed. The exclamation point is called the non-null assertion operator and it tells the Typescript compiler that the value of this property won't be null or undefined. 
