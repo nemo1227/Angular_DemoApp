d4e957f92055adafc55aca4c287bd0831c3c33b5
ShravaniB18 <shravaniborude03@gmail.com>
2025-10-14 12:31:00 +0530

    Updated

--- Frontend/src/app/landingpage/home/home.component.ts
+++ Frontend/src/app/landingpage/home/home.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  isMenuOpen = false;
-  isProfileMenuOpen = false;
-
-  constructor(private router: Router) {}
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-  toggleProfileMenu() {
-    this.isProfileMenuOpen = !this.isProfileMenuOpen;
-  }
-   onLogout() {
-    this.router.navigate(['/']);
-   }
+ 
 }
